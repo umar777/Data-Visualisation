96451554d622496021210b8d10b40da5638905c9
Omar <umarbinmuhammed@gmail.com>
2023-01-29 16:56:34 +0000

    Added

--- app.py
+++ app.py
@@ -9,50 +9,33 @@ pip.main(["install", "openpyxl"])
 st.set_page_config(page_title="My Plot", page_icon=None, layout="wide")
 
 
-grosvenor = pd.read_excel("Plot 10_Grosvenor_July&Aug.xlsx")
+grosvenor = pd.read_excel("grosvenor_plot10.xlsx")
+burton = pd.read_excel("burton_plot14.xlsx")
 
+st.title("Burton Plot 14 VS Grosvenor Road - Plot 10")
 
-new_header = grosvenor.iloc[0] #grab the first row for the header
-grosvenor = grosvenor[1:] #take the data less the header row
-grosvenor.columns = new_header #set the header row as the df header
+column_list_grosvenor = grosvenor.columns[2:] # assuming the first 2 columns are not the columns you want to plot
+column_list_burton = burton.columns[2:]
 
-st.title("Grosvenor Road - Plot 10")
+for column_G in column_list_grosvenor:
 
-column_list = grosvenor.columns[2:] # assuming the first 2 columns are not the columns you want to plot
+    for column_B in column_list_burton:
 
-weeks = [g for n, g in grosvenor.groupby(pd.Grouper(key='Date and Time',freq='W'))]
+        fig, ax = plt.subplots(figsize=(35, 10), dpi=1000)
 
-df_0 = weeks[0]
-df_1 = weeks[1]
-df_2 = weeks[2]
-df_3 = weeks[3]
-df_4 = weeks[4]
+        # plot data from data frame 1 in the first subplot
+        burton.plot(kind='line', x='Date_Time', y=column_B, marker='.', linestyle='', ax=ax, label='Burton Plot 14', color='red', x_compat=True)
 
-for column in column_list:
-    fig, ax = plt.subplots(figsize=(35, 10), dpi=1000)
+        # plot data from data frame 2 in the second subplot
+        grosvenor.plot(kind='line', x='Date and Time', y=column_G, marker='.', linestyle='', ax=ax, label='Grosvenor Plot 10', color='green', x_compat=True)
 
-    # plot data from data frame 1 in the first subplot
-    df_0.plot(kind='line', x='Date and Time', y=column, ax=ax, label='Week 1', x_compat=True)
+        plt.xlabel('Date and Time of the month - July')
+        plt.ylabel(column_B')
+        plt.legend()
 
-    # plot data from data frame 2 in the second subplot
-    df_1.plot(kind='line', x='Date and Time', y=column, ax=ax, label='Week 2', x_compat=True)
+        ax.xaxis.set_major_formatter(mdates.DateFormatter("%m.%d.%Y"))
+        ax.xaxis.set_major_locator(mdates.DayLocator(interval=1))
+        plt.gcf().autofmt_xdate()
 
-    # plot data from data frame 3 in the second subplot
-    df_2.plot(kind='line', x='Date and Time', y=column, ax=ax, label='Week 3', x_compat=True)
-
-    # plot data from data frame 4 in the second subplot
-    df_3.plot(kind='line', x='Date and Time', y=column, ax=ax, label='Week 4', x_compat=True)
-
-    # plot data from data frame 5 in the second subplot
-    df_4.plot(kind='line', x='Date and Time', y=column, ax=ax, label='Week 5', x_compat=True)
-
-    #grosvenor.plot(kind='line', x='Date and Time', y=column, ax=ax, label=column, x_compat=True)
-    plt.xlabel('Date and Time of the month - July')
-    plt.ylabel(column)
-
-    ax.xaxis.set_major_formatter(mdates.DateFormatter("%m.%d.%Y"))
-    ax.xaxis.set_major_locator(mdates.DayLocator(interval=1))
-    plt.gcf().autofmt_xdate()
-
-    st.subheader(column)
-    st.pyplot(fig)
+        st.subheader(column)
+        st.pyplot(fig)
